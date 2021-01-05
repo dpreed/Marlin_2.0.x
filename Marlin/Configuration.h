dd1ea758267c6a4ccc97a93013ce9a8109bc4be4
wilbur4321 <wilbur4321@gmail.com>
2021-01-05 15:03:45 -0800

    Multi-Z stepper inverting (#20678)
    Co-authored-by: Scott Lahteine <github@thinkyhead.com>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2628,7 +2628,7 @@
   //#define TOUCH_CALIBRATION_Y -8981
   //#define TOUCH_OFFSET_X        -43
   //#define TOUCH_OFFSET_Y        257
-  //#define TOUCH_ORIENTATION   TOUCH_LANDSCAPE
+  //#define TOUCH_ORIENTATION TOUCH_LANDSCAPE
 
   // QQS-Pro use MKS Robin TFT v2.0
   #define TOUCH_CALIBRATION_X 12033
